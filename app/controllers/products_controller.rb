120f29108e9535a8c409f645754a646c498f1706
Matt <matthewprimpas@gmail.com>
2018-02-05 21:05:22 -0500

    5.2 added functional search bar to the navigation

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -4,7 +4,14 @@ class ProductsController < ApplicationController
   # GET /products
   # GET /products.json
   def index
-    @products = Product.all
+    if params[:q]
+      search_term = params[:q]
+      @products = Product.where("name LIKE ?", "%#{search_term}%")
+      @products = Product.where("colour LIKE ?", "%#{search_term}%")
+      @products = Product.where("description LIKE ?", "%#{search_term}%")
+    else
+      @products = Product.all
+    end
   end
 
   # GET /products/1
