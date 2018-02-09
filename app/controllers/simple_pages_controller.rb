82e617f91fac890e1a17374ddcb464e02918524a
Matt <matthewprimpas@gmail.com>
2018-02-09 10:55:36 -0500

    5.3 created functional forms on the contact page that will send e-mail to customers

--- app/controllers/simple_pages_controller.rb
+++ app/controllers/simple_pages_controller.rb
@@ -5,4 +5,14 @@ class SimplePagesController < ApplicationController
   def landing_page
     @products = Product.limit(5)
   end
-end
+
+  def thank_you
+    @name = params[:name]
+    @email = params[:email]
+    @message = params[:message]
+    ActionMailer::Base.mail(from: @email,
+      to: 'matthewprimpas@gmail.com',
+      subject: "A new contact form message from #{@name}",
+      body: @message).deliver_now
+    end
+  end
