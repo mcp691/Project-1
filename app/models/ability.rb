f4061585a497bc587bd3737e1f981d19a75a325c
Matt <matthewprimpas@gmail.com>
2018-02-24 16:37:33 -0500

    add admin role with that gives permission to delete comments for admin users

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -4,6 +4,10 @@ class Ability
   def initialize(user)
     user ||- User.new # guest user (not logged in)
     can :manage, User, id: user.id
+
+    return unless user && user.admin?
+    can :delete, Comment
+    can :manage, :all  
   end
 
 end
