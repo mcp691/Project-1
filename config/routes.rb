e1d6c32d30fffdb1b67fc9fd5ae6237faf30af57
Matt <matthewprimpas@gmail.com>
2018-03-13 15:40:35 -0400

    add description to about us page

--- config/routes.rb
+++ config/routes.rb
@@ -17,6 +17,6 @@ Rails.application.routes.draw do
 
   post 'simple_pages/thank_you'
 
-  root 'simple_pages#index'
+  root 'simple_pages#landing_page'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
